d097af05a17481ad74a2f8802491e3de30f9d287
Juan Moreno <morenostudent1@gmail.com>
2021-06-28 13:35:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+// Define UI Variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners 
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners(){
+  // DOM load event
+  document.addEventListener('DOMcontentLoaded', getTasks);
+
+  // Add Task event
+  form.addEventListener('submit', addTask);
+  // Remove taask event
+  taskList.addEventListener('click', removeTask);
+  // Clear tasks
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter Tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Get Task from LS
+function getTasks(){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem(tasks));
+  }
+  tasks.forEach(function(task) {
+    // Create an li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(task));
+  // Create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+
+  // Add Icon html
+  link.innerHTML = '<i class= "fa fa-remove"></i>';
+
+  // Append the link to li
+  li.appendChild(link);
+
+  //Append li to ul
+  taskList.appendChild(li);
+  });
+}
+
+
+// Add task
+function addTask(e) {
+  if(taskInput.value === ''){
+    alert('Please add task');
+  }
+
+  // Create an li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+
+  // Add Icon html
+  link.innerHTML = '<i class= "fa fa-remove"></i>';
+
+  // Append the link to li
+  li.appendChild(link);
+
+  //Append li to ul
+  taskList.appendChild(li);
+
+  // Store in LS
+  storeTask(taskInput.value);
+
+
+// Clear input
+taskInput.value = '';
+  e.preventDefault();
+}
+
+// Store Task
+function storeTask(task){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+     if(confirm('Are you sure?')) { 
+       e.target.parentElement.parentElement.remove();
+
+       // Remove from LS
+       removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+      }
+    }
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach(function(task,index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1)
+    }
+  });
+  localStorage.setItem('task', JSON.stringify(tasks));
+}
+
+// Clear Tasks
+function clearTasks(){
+  // taskList.innerHTML = '';
+
+  // Faster way
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+
+    // Clear from LS
+    clearTasksFromLocalStorage();
+
+  }
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage(){
+  localStorage.clear();
+}
+
+// Filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+    }
+    else {
+      task.style.display = 'none';
+    }
+  });
+
+}
